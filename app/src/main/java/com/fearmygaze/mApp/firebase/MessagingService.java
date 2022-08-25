b7d70a70e6bdd1e1e90ae8cf976633f5acd9175c
Giorgos Papoulias <FearMyGaze@outlook.com>
2022-08-25 23:34:32 +0300

    Update on Messaging Service
    Now we store the token for messaging in the sharedPreferences

--- app/src/main/java/com/fearmygaze/mApp/firebase/MessagingService.java
+++ app/src/main/java/com/fearmygaze/mApp/firebase/MessagingService.java
@@ -1,9 +1,8 @@
 package com.fearmygaze.mApp.firebase;
 
-import android.util.Log;
-
 import androidx.annotation.NonNull;
 
+import com.fearmygaze.mApp.util.PrivatePreference;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -11,21 +10,13 @@ public class MessagingService extends FirebaseMessagingService {
 
     @Override
     public void onNewToken(@NonNull String token) {
-
-        /*
-         * TODO: sharedPreference the token and when the user login and after the login update the token
-         * */
-//        PrivatePreference preference = new PrivatePreference(getApplicationContext());
-//
-//        preference.putString("token",token);
-
+        PrivatePreference preference = new PrivatePreference(getApplicationContext());
+        preference.putString("token",token);
         super.onNewToken(token);
-        Log.d("FCM", "onNewToken: "+ token);
     }
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage message) {
         super.onMessageReceived(message);
-        Log.d("FCM", "onMessageReceived: " + message.getNotification().getBody());
     }
 }
