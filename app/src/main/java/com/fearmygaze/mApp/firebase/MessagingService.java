db23426829059f1e375c367ecb088996a9cf7c6d
Giorgos Papoulias <FearMyGaze@outlook.com>
2022-08-25 23:23:27 +0300

    Update MessagingService.java

--- app/src/main/java/com/fearmygaze/mApp/firebase/MessagingService.java
+++ app/src/main/java/com/fearmygaze/mApp/firebase/MessagingService.java
@@ -11,6 +11,14 @@ public class MessagingService extends FirebaseMessagingService {
 
     @Override
     public void onNewToken(@NonNull String token) {
+
+        /*
+         * TODO: sharedPreference the token and when the user login and after the login update the token
+         * */
+//        PrivatePreference preference = new PrivatePreference(getApplicationContext());
+//
+//        preference.putString("token",token);
+
         super.onNewToken(token);
         Log.d("FCM", "onNewToken: "+ token);
     }
