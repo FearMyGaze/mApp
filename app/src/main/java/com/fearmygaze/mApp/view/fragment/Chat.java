eb8546ef02266062059467dd7047256cfc6c0b9f
Giorgos Papoulias <FearMyGaze@outlook.com>
2022-09-05 21:14:57 +0300

    Update
    -Fix spacing

--- app/src/main/java/com/fearmygaze/mApp/view/fragment/Chat.java
+++ app/src/main/java/com/fearmygaze/mApp/view/fragment/Chat.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 public class Chat extends Fragment {
 
-    public Chat(User user){
+    public Chat(User user) {
         this.user = user;
     }
 
@@ -35,12 +35,6 @@ public class Chat extends Fragment {
 
     List<Conversation> conversationList;
 
-    /*
-    * TODO: When the conversation adapter is clicked, pass the appropriate information and maybe store them locally (Preferable)
-    *  so the new device doesn't have the customisations from the old one
-    *
-    * */
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -52,29 +46,29 @@ public class Chat extends Fragment {
         conversationList = new ArrayList<>();
 
         conversationList.add(new Conversation("1", "https://static-cdn.jtvnw.net/jtv_user_pictures/0d5d4ba9-881f-4d04-a9ae-b1ebe618442d-profile_image-70x70.png",
-                "AnneMunition","is this a bool ??","22:22"));
-        conversationList.add(new Conversation("2","https://static-cdn.jtvnw.net/jtv_user_pictures/fl0m-profile_image-efa66f8f4aa42f40-70x70.png",
-                "Fl0m","is this a String ??","22:20"));
-        conversationList.add(new Conversation("3","https://static-cdn.jtvnw.net/jtv_user_pictures/ae37597c-a887-4cb3-89f6-9fe7f3c16aa6-profile_image-70x70.png",
-                "AnneMunition","is this an int ??","22:20"));
-        conversationList.add(new Conversation("4","https://static-cdn.jtvnw.net/jtv_user_pictures/0d5d4ba9-881f-4d04-a9ae-b1ebe618442d-profile_image-70x70.png",
-                "AnneMunition","is this an int ??","22:20"));
-        conversationList.add(new Conversation("5","https://static-cdn.jtvnw.net/jtv_user_pictures/0d5d4ba9-881f-4d04-a9ae-b1ebe618442d-profile_image-70x70.png",
-                "AnneMunition","is this an int ??","22:20"));
-        conversationList.add(new Conversation("6","https://static-cdn.jtvnw.net/jtv_user_pictures/0d5d4ba9-881f-4d04-a9ae-b1ebe618442d-profile_image-70x70.png",
-                "AnneMunition","is this an int ??","22:20"));
-        conversationList.add(new Conversation("7","https://static-cdn.jtvnw.net/jtv_user_pictures/0d5d4ba9-881f-4d04-a9ae-b1ebe618442d-profile_image-70x70.png",
-                "AnneMunition","is this an int ??","22:20"));
-        conversationList.add(new Conversation("8","https://static-cdn.jtvnw.net/jtv_user_pictures/0d5d4ba9-881f-4d04-a9ae-b1ebe618442d-profile_image-70x70.png",
-                "AnneMunition","is this an int ??","22:20"));
-        conversationList.add(new Conversation("9","https://static-cdn.jtvnw.net/jtv_user_pictures/0d5d4ba9-881f-4d04-a9ae-b1ebe618442d-profile_image-70x70.png",
-                "AnneMunition","is this an int ??","22:20"));
-        conversationList.add(new Conversation("10","https://static-cdn.jtvnw.net/jtv_user_pictures/0d5d4ba9-881f-4d04-a9ae-b1ebe618442d-profile_image-70x70.png",
-                "AnneMunition","is this an int ??","22:20"));
-        conversationList.add(new Conversation("11","https://static-cdn.jtvnw.net/jtv_user_pictures/0d5d4ba9-881f-4d04-a9ae-b1ebe618442d-profile_image-70x70.png",
-                "AnneMunition","is this an int ??","22:20"));
-        conversationList.add(new Conversation("12","https://static-cdn.jtvnw.net/jtv_user_pictures/0d5d4ba9-881f-4d04-a9ae-b1ebe618442d-profile_image-70x70.png",
-                "AnneMunition","is this an int ??","22:20"));
+                "AnneMunition", "is this a bool ??", "22:22"));
+        conversationList.add(new Conversation("2", "https://static-cdn.jtvnw.net/jtv_user_pictures/fl0m-profile_image-efa66f8f4aa42f40-70x70.png",
+                "Fl0m", "is this a String ??", "22:20"));
+        conversationList.add(new Conversation("3", "https://static-cdn.jtvnw.net/jtv_user_pictures/d96af87b-949c-4074-ace1-48c1f94533b6-profile_image-70x70.png",
+                "ThePrimeagen", "is this Rust ??", "22:20"));
+        conversationList.add(new Conversation("4", "https://static-cdn.jtvnw.net/jtv_user_pictures/0d5d4ba9-881f-4d04-a9ae-b1ebe618442d-profile_image-70x70.png",
+                "AnneMunition", "is this an int ??", "22:20"));
+        conversationList.add(new Conversation("5", "https://static-cdn.jtvnw.net/jtv_user_pictures/0d5d4ba9-881f-4d04-a9ae-b1ebe618442d-profile_image-70x70.png",
+                "AnneMunition", "is this an int ??", "22:20"));
+        conversationList.add(new Conversation("6", "https://static-cdn.jtvnw.net/jtv_user_pictures/0d5d4ba9-881f-4d04-a9ae-b1ebe618442d-profile_image-70x70.png",
+                "AnneMunition", "is this an int ??", "22:20"));
+        conversationList.add(new Conversation("7", "https://static-cdn.jtvnw.net/jtv_user_pictures/0d5d4ba9-881f-4d04-a9ae-b1ebe618442d-profile_image-70x70.png",
+                "AnneMunition", "is this an int ??", "22:20"));
+        conversationList.add(new Conversation("8", "https://static-cdn.jtvnw.net/jtv_user_pictures/0d5d4ba9-881f-4d04-a9ae-b1ebe618442d-profile_image-70x70.png",
+                "AnneMunition", "is this an int ??", "22:20"));
+        conversationList.add(new Conversation("9", "https://static-cdn.jtvnw.net/jtv_user_pictures/0d5d4ba9-881f-4d04-a9ae-b1ebe618442d-profile_image-70x70.png",
+                "AnneMunition", "is this an int ??", "22:20"));
+        conversationList.add(new Conversation("10", "https://static-cdn.jtvnw.net/jtv_user_pictures/0d5d4ba9-881f-4d04-a9ae-b1ebe618442d-profile_image-70x70.png",
+                "AnneMunition", "is this an int ??", "22:20"));
+        conversationList.add(new Conversation("11", "https://static-cdn.jtvnw.net/jtv_user_pictures/0d5d4ba9-881f-4d04-a9ae-b1ebe618442d-profile_image-70x70.png",
+                "AnneMunition", "is this an int ??", "22:20"));
+        conversationList.add(new Conversation("12", "https://static-cdn.jtvnw.net/jtv_user_pictures/0d5d4ba9-881f-4d04-a9ae-b1ebe618442d-profile_image-70x70.png",
+                "AnneMunition", "is this an int ??", "22:20"));
 
 
         adapterConversation = new AdapterConversation(conversationList, requireActivity());
