5701bb3e57326b8c5c9baad45b48ad838fc7d4e1
Giorgos Papoulias <FearMyGaze@outlook.com>
2022-09-13 19:26:29 +0300

    Added chatRoomSettings

--- app/src/main/java/com/fearmygaze/mApp/view/activity/ChatRoom.java
+++ app/src/main/java/com/fearmygaze/mApp/view/activity/ChatRoom.java
@@ -1,5 +1,6 @@
 package com.fearmygaze.mApp.view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
 
@@ -28,6 +29,13 @@ public class ChatRoom extends AppCompatActivity {
 
         toolbar.setNavigationOnClickListener(v -> onBackPressed());
         toolbar.setTitle(getIntent().getStringExtra("username"));
+        toolbar.setOnMenuItemClickListener(item -> {
+            if (item.getItemId() == R.id.chatRoomToolbarItemInfo){
+                startActivity(new Intent(ChatRoom.this, ChatRoomSettings.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            }
+            return true;
+        });
 
         imageButton.setOnClickListener(v -> {
 
